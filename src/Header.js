2f0e5d2fb2efa33d5dd65da6f9c4a637eb10dc5d
Fezan Muhammad Ali <callmetheengcountio@gmail.com>
2024-03-23 09:22:18 +0500

    Refactored the App component has created the 3 compoenent Header,Main and Archive from App to minimize code size in App

---
+++ src/Header.js
@@ -0,0 +1,33 @@
+function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
+  return (
+    <header>
+      <h1>
+        <span>⚛️</span>The Atomic Blog
+      </h1>
+      <div>
+        <Results posts={posts} />
+        <SearchPosts
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+        />
+        <button onClick={onClearPosts}>Clear posts</button>
+      </div>
+    </header>
+  );
+}
+
+function SearchPosts({ searchQuery, setSearchQuery }) {
+  return (
+    <input
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      placeholder="Search posts..."
+    />
+  );
+}
+
+function Results({ posts }) {
+  return <p>🚀 {posts.length} atomic posts found</p>;
+}
+
+export default Header;
